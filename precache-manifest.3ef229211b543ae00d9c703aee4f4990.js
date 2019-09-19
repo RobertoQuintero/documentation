929e58354913b01ed4b49b590745a1057f638d35
Roberto Quintero <langdonhp435@gmail.com>
2019-09-19 17:03:07 -0500

    Updates

---
+++ precache-manifest.3ef229211b543ae00d9c703aee4f4990.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba48e6101be4156cce1d386a4879ee78",
+    "url": "/documentation/index.html"
+  },
+  {
+    "revision": "fc06302f6f1d2269b68e",
+    "url": "/documentation/static/css/main.2939daee.chunk.css"
+  },
+  {
+    "revision": "73f7440a8c20e7f646e2",
+    "url": "/documentation/static/js/2.9c47a423.chunk.js"
+  },
+  {
+    "revision": "fc06302f6f1d2269b68e",
+    "url": "/documentation/static/js/main.d2f29724.chunk.js"
+  },
+  {
+    "revision": "12d0bca2e99292769345",
+    "url": "/documentation/static/js/runtime~main.77a99e7d.js"
+  },
+  {
+    "revision": "9762cb0c74da2c22b2a8a2a0029e0e85",
+    "url": "/documentation/static/media/1-banner.9762cb0c.png"
+  },
+  {
+    "revision": "4506ce2d8f53dc24bbe815f5a075f29b",
+    "url": "/documentation/static/media/10-g3.4506ce2d.jpg"
+  },
+  {
+    "revision": "94049756b21c63bab376740ebf891e65",
+    "url": "/documentation/static/media/13-g6.94049756.jpg"
+  },
+  {
+    "revision": "bf7472a281a892cf2a2ed2d7f9475ea3",
+    "url": "/documentation/static/media/14-g7.bf7472a2.jpg"
+  },
+  {
+    "revision": "8abeb7172d33338bdb14af811852a229",
+    "url": "/documentation/static/media/15-g8.8abeb717.jpg"
+  },
+  {
+    "revision": "38d0b55b5671fd0338d7a0343c0f9b2e",
+    "url": "/documentation/static/media/16-r1.38d0b55b.png"
+  },
+  {
+    "revision": "b6a23c9dc68e05f9bfa344f872591bb3",
+    "url": "/documentation/static/media/2-about.b6a23c9d.jpg"
+  },
+  {
+    "revision": "2783341d71ee5ba207ae5fbea69a3560",
+    "url": "/documentation/static/media/3-about.2783341d.png"
+  },
+  {
+    "revision": "810f159352aa9dd7d1140f6d3a5daf79",
+    "url": "/documentation/static/media/4-team1.810f1593.jpg"
+  },
+  {
+    "revision": "354e3875743cae49a9e6aa2e3c5fd249",
+    "url": "/documentation/static/media/5-team2.354e3875.jpg"
+  },
+  {
+    "revision": "9cbfda7f4e339da451b98a6d9939564f",
+    "url": "/documentation/static/media/6-team3.9cbfda7f.jpg"
+  },
+  {
+    "revision": "383c3b5b95baf45f0ed663f39556df91",
+    "url": "/documentation/static/media/7-team4.383c3b5b.jpg"
+  },
+  {
+    "revision": "66e766a09e69afbbb9e171b281414ea4",
+    "url": "/documentation/static/media/8-g1.66e766a0.jpg"
+  },
+  {
+    "revision": "41b9bfd80e2a25c84ebac514790593fd",
+    "url": "/documentation/static/media/9-g2.41b9bfd8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.20da4dcc6f0024d04efeb05661a7a4b0.js"
+  "/documentation/precache-manifest.3ef229211b543ae00d9c703aee4f4990.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/documentation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
